004330a32baabdd44600149ad40478418aaaeede
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2024-05-23 17:18:54 +0700

    adding app-background.jpg & user-profile-background.jpg, editing pubspec.yaml and adding user_profile.dart in user_profile in lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:labbi_frontend/user_profile/user_profile.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.red,
+//           title: const Text("My App"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+ 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: const Text("My App"),
-        ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHome()
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Open Route'),
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const UserProfilePage()));
+        },
       ),
     );
   }
 }
- 
