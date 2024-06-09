930f6deb067b8fe4c37085d9a5e0787962444f59
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2024-06-09 21:36:51 +0700

    add some contents to user_profile.dart

--- lib/user_profile/user_profile.dart
+++ lib/user_profile/user_profile.dart
@@ -5,40 +5,107 @@ class UserProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    dynamic screenHeight = MediaQuery.of(context).size.height;
+    dynamic screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: null,
-      // AppBar(
-      //   title: const Text('Testing'),
-      // ),
       body: Stack(
+        clipBehavior: Clip.none,
         children: <Widget>[
           Container(
-            decoration: const BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/user-profile-background.jpg"), fit: BoxFit.cover)),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image:
+                        AssetImage("assets/images/user-profile-background.jpg"),
+                    fit: BoxFit.cover)),
           ),
-          SingleChildScrollView(
-            child: Container(
-              height: (1/3) * MediaQuery.of(context).size.height,
-              decoration: const BoxDecoration(
-                image: DecorationImage(image: AssetImage("assets/images/app-background.jpg"), fit: BoxFit.cover)
+          userCoverImage(context, screenHeight, screenWidth),
+          Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: ((0.8 / 3) * screenHeight) - (screenHeight / 10),
+                child: userProfileImage(context, screenHeight, screenWidth),
               ),
+              Positioned(
+                top: ((0.8 / 3) * screenHeight) + (screenHeight / 8.6),
+                child: userDetail(context, screenHeight, screenWidth),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  /*Cover image*/
+  Widget userCoverImage(context, screenHeight, screenWidth) => Container(
+        height: (0.8 / 3) * screenHeight,
+        width: screenWidth,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/app-background.jpg"),
+                fit: BoxFit.cover)),
+      );
+
+  /*Profile Image*/
+  Widget userProfileImage(context, screenHeight, screenWidth) => Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+                blurRadius: 8,
+                color: Color(0xff0094ff),
+                spreadRadius: 0,
+                offset: Offset(0, 4))
+          ],
+        ),
+        child: CircleAvatar(
+          backgroundColor: Colors.white,
+          radius: screenHeight / 9.7,
+          child: CircleAvatar(
+            radius: screenHeight / 10.3,
+            child: Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/user-profile.png"))),
+            ),
+          ),
+        ),
+      );
+
+  /*User Information*/
+  Widget userDetail(context, screenHeight, screenWidth) => Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.center,
+        children: [
+          const Positioned(
+            child: Text(
+              "Username",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Positioned(
+              top: 25,
+              child: Text(
+                "Administrator",
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              )),
+          Positioned(
+            top: 64,
+            child: Container(
+              height: screenHeight * (2 / 5),
+              width: (9 / 10) * screenWidth,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(blurRadius: 8, color: Colors.black, spreadRadius: 0.5, offset: Offset(0, 4))
+                  ],
+                  // border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(20)),
             ),
           )
         ],
-      ),
-      
-      // Container(
-      //   // decoration: const BoxDecoration(
-      //   //   image: DecorationImage(
-      //   //     image: AssetImage("assets/images/app-background-1.jpg"), fit: BoxFit.cover
-      //   //   )
-      //   // ),
-      //   // child: Container (
-      //   //   height: (2/3) * MediaQuery.of(context).size.height,
-      //   //   decoration: const BoxDecoration(
-      //   //     image: DecorationImage(image: AssetImage("assets/images/app-background-2.jpg"), fit: BoxFit.cover)
-      //   //   ),
-      //   // ),
-      // ),
-    );
-  }
+      );
 }
