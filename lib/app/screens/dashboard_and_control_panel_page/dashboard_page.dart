1441dbb41c1321484af4ff7fec20c31b7bb83a7f
minh0811 <vokhaiminh0811@gmail.com>
2024-08-04 18:54:07 +0700

    Add partial tasks

--- lib/app/screens/dashboard_and_control_panel_page/dashboard_page.dart
+++ lib/app/screens/dashboard_and_control_panel_page/dashboard_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:labbi_frontend/app/screens/menu/nav_bar.dart';
 import 'package:labbi_frontend/app/widgets/custom_search_delegate.dart';
 import 'package:labbi_frontend/app/models/dashboard.dart';
-import 'package:labbi_frontend/app/screens/dashboard_and_control_panel_page/dashboard_items.dart ';
+import 'package:labbi_frontend/app/screens/dashboard_and_control_panel_page/dashboard_items.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ import 'package:labbi_frontend/app/screens/start_page/start_page.dart';
 // import 'authentication/register/register.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:labbi_frontend/app/screens/dashboard_and_control_panel_page/dashboard_page.dart';
+import 'app/screens/menu/nav_bar.dart';
+import 'app/screens/notification/notification_page.dart';
+import 'app/screens/start_page/start_page.dart';
+import 'app/screens/user_org/user_home_org.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,9 +28,22 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: token != null && !JwtDecoder.isExpired(token!)
           ? Dashboard(token: token!)
-          : const DashboardPage(), // Check for null and use token safely
+          : const UserHomeOrg(), // Check for null and use token safely
           // : const UserHomeOrg(), // testing to check
           // : const UserProfileUpdate(), // testing to check
     );
   }
 }
+
+//Normal user 
+//sau khi login, page đầu tiên cua user la dashboard_page. 
+//TODO: trong menu, 3 options: user profile, dashboard, notification 
+//TODO: trong menu sau khi đi tới user profile sẽ đi tới  edit user profile 
+//TODO: trong menu, dashboard sẽ đi tới dashboard_page
+//TODO: trong menu, notification sẽ đi tới notification_page
+//TODO: xoá message trong Menu 
+
+//Admin user
+//sau khi login, page đầu tiên của admin là admin admin_dashboard_page (hiện tại thiếu admin_dashboard_page)
+//TODO: trong menu Admin, 3 options: user profile, admin_dashboard_page, admin notification ()
+
