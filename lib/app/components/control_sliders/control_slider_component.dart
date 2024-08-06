aa962ead2c9dec189f2ce663dab456537cf004ca
minh0811 <vokhaiminh0811@gmail.com>
2024-08-06 21:40:15 +0700

    Add control panel page

---
+++ lib/app/components/control_sliders/control_slider_component.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ControlSliderComponent extends StatefulWidget {
+  final IconData icon;
+  final String label;
+  final double value;
+  final ValueChanged<double> onChanged;
+  final double min;
+  final double max;
+
+  ControlSliderComponent({
+    required this.icon,
+    required this.label,
+    required this.value,
+    required this.onChanged,
+    this.min = 0,
+    this.max = 1000,
+  });
+
+  @override
+  _ControlSliderState createState() => _ControlSliderState();
+}
+
+class _ControlSliderState extends State<ControlSliderComponent> {
+  double _currentValue = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentValue = widget.value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(widget.icon, size: 30),
+        SizedBox(width: 10),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(widget.label),
+              Slider(
+                value: _currentValue,
+                min: widget.min,
+                max: widget.max,
+                onChanged: (value) {
+                  setState(() {
+                    _currentValue = value;
+                  });
+                  widget.onChanged(value);
+                },
+              ),
+            ],
+          ),
+        ),
+        Icon(Icons.arrow_forward, size: 30),
+      ],
+    );
+  }
+}
--- lib/app/screens/control_panel_page/control_panel_page.dart
+++ lib/app/screens/control_panel_page/control_panel_page.dart
@@ -3,6 +3,8 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:labbi_frontend/app/components/charts/two_line_chart_component.dart';
+import 'package:labbi_frontend/app/components/control_sliders/control_slider_component.dart';
+import 'package:labbi_frontend/app/mock_datas/device_data.dart'; // Import the device data
 
 class ControlPanelPage extends StatelessWidget {
   @override
@@ -17,31 +19,22 @@ class ControlPanelPage extends StatelessWidget {
           },
         ),
       ),
-      body: ListView(
+      body: ListView.builder(
         padding: EdgeInsets.all(10),
-        children: [
-          DeviceCard(
-            deviceName: 'MFZWI3D',
-            status: 'online',
-            version: '1.3.4',
-            humidityData: [5.0, 4.0, 6.0, 5.0, 7.0, 6.5, 7.5],
-            temperatureData: [6.0, 7.0, 5.0, 6.5, 5.0, 4.0, 3.0],
-            motorSpeed: 200,
-            fanSpeed: 1000,
-            isActive: true,
-          ),
-          SizedBox(height: 10),
-          DeviceCard(
-            deviceName: 'MSDFGW',
-            status: 'offline',
-            version: '1.1.2',
-            humidityData: [],
-            temperatureData: [],
-            motorSpeed: 0,
-            fanSpeed: 0,
-            isActive: false,
-          ),
-        ],
+        itemCount: devices.length,
+        itemBuilder: (context, index) {
+          final device = devices[index];
+          return DeviceCard(
+            deviceName: device.deviceName,
+            status: device.status,
+            version: device.version,
+            humidityData: device.humidityData,
+            temperatureData: device.temperatureData,
+            motorSpeed: device.motorSpeed,
+            fanSpeed: device.fanSpeed,
+            isActive: device.isActive,
+          );
+        },
       ),
     );
   }
@@ -120,7 +113,7 @@ class DeviceCard extends StatelessWidget {
                     temperatureData: temperatureData)
                 : Center(child: Text('No data available')),
             SizedBox(height: 10),
-            ControlSlider(
+            ControlSliderComponent(
               icon: FontAwesomeIcons.cog,
               label: 'Motor speed: $motorSpeed RPM',
               value: motorSpeed.toDouble(),
@@ -128,7 +121,7 @@ class DeviceCard extends StatelessWidget {
                 // Handle motor speed change
               },
             ),
-            ControlSlider(
+            ControlSliderComponent(
               icon: FontAwesomeIcons.fan,
               label: 'Fan speed: $fanSpeed RPM',
               value: fanSpeed.toDouble(),
@@ -139,45 +132,6 @@ class DeviceCard extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class ControlSlider extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  final double value;
-  final ValueChanged<double> onChanged;
-
-  ControlSlider({
-    required this.icon,
-    required this.label,
-    required this.value,
-    required this.onChanged,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(icon, size: 30),
-        SizedBox(width: 10),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(label),
-              Slider(
-                value: value,
-                min: 0,
-                max: 1000,
-                onChanged: onChanged,
-              ),
-            ],
-          ),
-        ),
-        Icon(Icons.arrow_forward, size: 30),
-      ],
     );
   }
 }
