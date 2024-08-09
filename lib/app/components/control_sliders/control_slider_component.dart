c4ab82923a6082f92b1857e06083756ad6354618
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2024-08-09 20:58:47 +0700

    link fan & motor popups to control panel page

--- lib/app/components/control_sliders/control_slider_component.dart
+++ lib/app/components/control_sliders/control_slider_component.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:labbi_frontend/app/screens/control_panel_page/fan_popup.dart';
+import 'package:labbi_frontend/app/screens/control_panel_page/motor_popup.dart';
 
 class ControlSliderComponent extends StatefulWidget {
   final IconData icon;
@@ -56,7 +58,21 @@ class _ControlSliderState extends State<ControlSliderComponent> {
             ],
           ),
         ),
-        Icon(Icons.arrow_forward, size: 30),
+        InkWell(
+          onTap: () {
+            if (widget.icon == FontAwesomeIcons.fan) {
+              showDialog(
+                  context: context,
+                  builder: (context) => FanPopup(currentValue: _currentValue));
+            } else {
+              showDialog(
+                  context: context,
+                  builder: (context) =>
+                      MotorPopup(currentValue: _currentValue));
+            }
+          },
+          child: const Icon(Icons.arrow_forward, size: 30),
+        )
       ],
     );
   }
