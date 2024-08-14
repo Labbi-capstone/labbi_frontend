aaa5a5f0331493528b45403a007c42a6460a458b
Võ Khải Minh <vokhaiminh0811@gmail.com>
2024-08-14 19:11:35 +0700

    revert to current work

--- lib/app/screens/user_org/list_user_device.dart
+++ lib/app/screens/user_org/list_user_device.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:labbi_frontend/app/models/user_device_test.dart';
 import 'package:labbi_frontend/app/screens/user_org/device_details.dart';
 
-class ListUserDevice extends StatefulWidget {
+class ListUserDevice extends StatefulWidget{
+  
   final List<UserDevice> devices;
 
   const ListUserDevice({super.key, required this.devices});
@@ -12,8 +13,8 @@ class ListUserDevice extends StatefulWidget {
   // ignore: library_private_types_in_public_api
   _ListUserDeviceState createState() => _ListUserDeviceState();
 }
-
-class _ListUserDeviceState extends State<ListUserDevice> {
+  
+class _ListUserDeviceState extends State<ListUserDevice>{
   @override
   Widget build(BuildContext context) {
     dynamic screenHeight = MediaQuery.of(context).size.height;
@@ -23,21 +24,19 @@ class _ListUserDeviceState extends State<ListUserDevice> {
       itemCount: widget.devices.length,
       itemBuilder: (context, index) {
         return Padding(
-          padding: EdgeInsets.symmetric(vertical: screenHeight * 0.004),
+          padding: EdgeInsets.symmetric(vertical: screenHeight*0.004),
           child: Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(7),
             ),
             elevation: 4,
             child: ListTile(
-              minTileHeight: screenHeight * 0.09,
-              title: Row(
+              minTileHeight: screenHeight*0.09,
+               title: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Icon(Icons.memory,
-                      size: screenHeight * 0.1, color: Colors.black),
-                  SizedBox(
-                      width: screenWidth * 0.04), // Space between icon and text
+                  Icon(Icons.memory, size: screenHeight * 0.1, color: Colors.black),
+                  SizedBox(width: screenWidth*0.04), // Space between icon and text
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -45,8 +44,7 @@ class _ListUserDeviceState extends State<ListUserDevice> {
                         widget.devices[index].name,
                         style: const TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      Text(
-                          'ID: ${widget.devices[index].id}\nStatus: ${widget.devices[index].status}'),
+                      Text('ID: ${widget.devices[index].id}\nStatus: ${widget.devices[index].status}'),
                     ],
                   ),
                 ],
@@ -55,8 +53,7 @@ class _ListUserDeviceState extends State<ListUserDevice> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) =>
-                        DeviceDetails(device: widget.devices[index]),
+                    builder: (context) => DeviceDetails(device: widget.devices[index]),
                   ),
                 );
               },
--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,19 @@
 import 'package:flutter/material.dart';
-
-import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:labbi_frontend/app/screens/start_page/start_page.dart';
-// import 'authentication/register/register.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:labbi_frontend/app/screens/dashboard_page/dashboard_page.dart';
-import 'app/screens/menu/nav_bar.dart';
-import 'app/screens/notification/notification_page.dart';
-import 'app/screens/start_page/start_page.dart';
-import 'app/screens/user_org/user_home_org.dart';
-import 'app/screens/authentication/login/login_page.dart';
-import 'app/screens/control_panel_page/control_panel_page.dart';
-//import 'app/screens/admin_org/admin_home_org.dart';
-import 'package:labbi_frontend/app/screens/admin_org/admin_home_org.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   SharedPreferences prefs = await SharedPreferences.getInstance();
-
-//   dynamic token = prefs.getString('token');
-
-//   runApp(MyApp(token: token));
-// }
-
-// class MyApp extends StatelessWidget {
-//   final dynamic token;
-//   const MyApp({@required this.token, super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: token != null && !JwtDecoder.isExpired(token!)
-//           ? Dashboard(token: token!)
-//           : ControlPanelPage(), // Check for null and use token safely
-//       // : const AdminHomeOrg(),
-//       // : const UserHomeOrg(), // testing to check
-//       // : const UserProfileUpdate(), // testing to check
-//     );
-//   }
-// }
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-
-  dynamic token = prefs.getString('token');
-
-  runApp(MyApp(token: token));
+// Import the Prometheus data page
+import 'package:labbi_frontend/display.dart';
+import 'package:labbi_frontend/cpuDisplay.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+Future<void> main() async {
+   await dotenv.load(fileName: ".env");
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String? token;
-  const MyApp({this.token, super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AdminHomeOrg(),
+      home: CPUUsagePage(), // Directly set PrometheusDataPage as the home
     );
   }
-
-  Widget _getInitialPage() {
-    if (token != null && !JwtDecoder.isExpired(token!)) {
-      // You can add more checks here if needed, e.g., user roles, etc.
-      return Dashboard(token: token!);
-    } else {
-      return const AdminHomeOrg();
-      // return const UserHomeOrg(); // Replace with the appropriate initial page if token is null or expired
-    }
-  }
 }
-//Normal user 
-//sau khi login, page đầu tiên cua user la dashboard_page. 
-//TODO: trong menu, 3 options: user profile, dashboard, notification 
-//TODO: trong menu sau khi đi tới user profile sẽ đi tới  edit user profile 
-//TODO: trong menu, dashboard sẽ đi tới dashboard_page
-//TODO: trong menu, notification sẽ đi tới notification_page
-//TODO: xoá message trong Menu 
-
-//Admin user
-//sau khi login, page đầu tiên của admin là admin admin_dashboard_page (hiện tại thiếu admin_dashboard_page)
-//TODO: trong menu Admin, 3 options: user profile, admin_dashboard_page, admin notification ()
