36eb1b124296341070e7430054bd76f3983c6317
Võ Khải Minh <vokhaiminh0811@gmail.com>
2024-08-18 04:00:27 +0700

    Refine login logic, refine menu code for better readable

---
+++ lib/app/routes.dart
@@ -0,0 +1,11 @@
+// routes.dart
+class Routes {
+  //static const String start = '/start';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String dashboard = '/dashboard';
+  static const String start = '/start';
+  static const String UserOrgHomePage = '/UserOrgHomePage';
+
+  // Add more routes as needed
+}
--- lib/app/screens/menu/menu_task_bar.dart
+++ lib/app/screens/menu/menu_task_bar.dart
@@ -1,80 +1,125 @@
 import 'package:flutter/material.dart';
-import 'package:labbi_frontend/app/screens/menu/nav_buttons_list.dart';
+import 'package:labbi_frontend/app/components/logout_button.dart';
+import 'package:labbi_frontend/app/models/menu_item_model.dart';
 import 'package:labbi_frontend/app/screens/user_profile/user_profile.dart';
 
-class MenuTaskbar extends StatefulWidget {
+class MenuTaskbar extends StatelessWidget {
   const MenuTaskbar({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _MenuTaskbarState createState() => _MenuTaskbarState();
-}
-
-class _MenuTaskbarState extends State<MenuTaskbar>{
-  
-  final userName = 'User';
-  final String pathImage = 'assets/images/man.png';
-  
-  @override
   Widget build(BuildContext context) {
-    dynamic screenHeight = MediaQuery.of(context).size.height;
-    dynamic screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
 
     return Drawer(
-      width: screenWidth*0.7,
+      width: screenWidth * 0.7,
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02,vertical: screenHeight * 0.02),
+        padding: EdgeInsets.symmetric(
+            horizontal: screenWidth * 0.02, vertical: screenHeight * 0.02),
         child: Column(
           children: [
+            // Menu Icon to close the drawer
             Row(
               children: [
                 IconButton(
                   onPressed: () {
-                    Navigator.pop(context); // Navigate back to the previous screen
+                    Navigator.pop(context); // Close the drawer
                   },
-                  icon: const Icon(Icons.menu, color: Colors.black,),
+                  icon: const Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                  ),
                 ),
               ],
             ),
 
-            // Line
-            const Divider(
-              color: Colors.black,
-            ),
+            const Divider(color: Colors.black),
 
-            // User Profile
+            // User Profile Section
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const UserProfilePage(listOfNotification: [],)),
-              );
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const UserProfilePage(
+                      listOfNotification: [],
+                    ),
+                  ),
+                );
               },
               style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth*0.02,vertical: screenHeight*0.02), // Remove default padding
+                padding: EdgeInsets.symmetric(
+                    horizontal: screenWidth * 0.02,
+                    vertical: screenHeight * 0.02),
                 shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.zero,
                 ),
               ),
               child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
                   CircleAvatar(
                     radius: screenHeight * 0.055,
-                    child: Image.asset(pathImage),
+                    child: Image.asset('assets/images/man.png'),
                   ),
                   SizedBox(width: screenWidth * 0.05),
-                  Text(userName, style: TextStyle(fontSize: screenHeight * 0.03, fontWeight: FontWeight.bold)),
+                  Text(
+                    'User',
+                    style: TextStyle(
+                      fontSize: screenHeight * 0.03,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ],
               ),
             ),
 
-            // Frame for a list of buttons
-            const Expanded(
-              child: NavButtonsList()
+            // Menu Items
+            Expanded(
+              child: ListView.builder(
+                itemCount: userMenuItems.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: screenHeight * 0.004),
+                    child: TextButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => userMenuItems[index].route,
+                          ),
+                        );
+                      },
+                      icon: Padding(
+                        padding: EdgeInsets.only(right: screenWidth * 0.02),
+                        child: Icon(userMenuItems[index].icon,
+                            size: screenHeight * 0.08),
+                      ),
+                      label: Text(
+                        userMenuItems[index].label,
+                        style: TextStyle(fontSize: screenHeight * 0.02),
+                      ),
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
 
-          ]
+            const Divider(color: Colors.black),
+
+            // Log out Button
+            SizedBox(
+              width: screenWidth,
+              child: const LogoutButton(),
+            ),
+          ],
         ),
       ),
     );
