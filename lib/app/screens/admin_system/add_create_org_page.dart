6cb67207f1dd02127004e7368485de3755dfd909
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2024-08-28 16:20:00 +0700

    change background color and adjust some components on create org page

--- lib/app/screens/admin_system/add_create_org_page.dart
+++ lib/app/screens/admin_system/add_create_org_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:labbi_frontend/app/controllers/org_controller.dart';
 import 'package:provider/provider.dart';
-
+import 'package:labbi_frontend/app/Theme/app_colors.dart';
 
 class AddCreateOrgPage extends StatefulWidget {
   const AddCreateOrgPage({super.key});
@@ -24,7 +24,20 @@ class _AddCreateOrgPageState extends State<AddCreateOrgPage> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: const Color(0xff3ac7f9),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                AppColors.primary,
+                AppColors.secondary,
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
         centerTitle: true,
         leading: IconButton(
           icon: const Icon(
@@ -41,129 +54,162 @@ class _AddCreateOrgPageState extends State<AddCreateOrgPage> {
               fontSize: screenHeight / 35),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: screenWidth,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                      "assets/images/create-dashboard-background.jpg"),
-                  fit: BoxFit.fill)),
-          child: Padding(
-            padding: EdgeInsets.only(
-              top: 0.3 * screenHeight,
-              bottom: 0.05 * screenHeight,
-              left: 0.05 * screenWidth,
-              right: 0.05 * screenWidth,
+      body: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            height: screenHeight,
+            width: screenWidth,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  AppColors.primary,
+                  AppColors.secondary,
+                ],
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp,
+              ),
             ),
-            child: Form(
-              key: _formKey,
-              child: Container(
-                height: 350,
-                width: (9 / 10) * screenWidth,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                          top: 0.03 * screenHeight,
-                          left: 0.07 * screenWidth,
-                          right: 0.07 * screenWidth),
-                      child: Text(
-                        'Organization\'s Information',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: screenHeight * 0.028),
-                      ),
-                    ),
-                    // Organization Name Input
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 0.07 * screenWidth,
-                          vertical: 0.02 * screenHeight),
-                      child: TextFormField(
-                        controller: _orgNameController,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Enter organization name',
+          ),
+          /*Components in cover image */
+          SizedBox(
+            height: (1 / 3) * screenHeight,
+            width: screenWidth,
+            child: Container(
+              height: null,
+              width: screenWidth,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/company-logo-white.png"),
+                      fit: BoxFit.fill)),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 0.3 * screenHeight,
+                bottom: 0.05 * screenHeight,
+                left: 0.05 * screenWidth,
+                right: 0.05 * screenWidth,
+              ),
+              child: Form(
+                key: _formKey,
+                child: Container(
+                  height: null,
+                  width: (9 / 10) * screenWidth,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: 0.03 * screenHeight,
+                            left: 0.07 * screenWidth,
+                            right: 0.07 * screenWidth),
+                        child: Text(
+                          'Organization\'s Information',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: screenHeight * 0.028),
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter the organization name';
-                          }
-                          return null;
-                        },
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 0.03 * screenHeight,
-                          horizontal: 0.07 * screenWidth),
-                      child: const Divider(color: Colors.grey),
-                    ),
-                    // Error Message (if any)
-                    if (orgController.errorMessage != null)
+                      // Organization Name Input
                       Padding(
                         padding: EdgeInsets.symmetric(
-                            vertical: 0.01 * screenHeight,
-                            horizontal: 0.07 * screenWidth),
-                        child: Text(
-                          orgController.errorMessage!,
-                          style: TextStyle(color: Colors.red),
-                        ),
-                      ),
-                    // Create Button
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 0.03 * screenHeight,
-                      ),
-                      child: Container(
-                        height: 0.07 * screenHeight,
-                        width: (5 / 10) * screenWidth,
-                        decoration: BoxDecoration(
-                          color: const Color(0xff3ac7f9),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            backgroundColor: const Color(0xff3ac7f9),
+                            horizontal: 0.07 * screenWidth,
+                            vertical: 0.02 * screenHeight),
+                        child: TextFormField(
+                          controller: _orgNameController,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter organization name',
                           ),
-                          onPressed: orgController.isLoading
-                              ? null
-                              : () {
-                                  if (_formKey.currentState!.validate()) {
-                                    orgController.createOrganization(
-                                      _orgNameController.text,
-                                      context,
-                                    );
-                                  }
-                                },
-                          child: orgController.isLoading
-                              ? CircularProgressIndicator(
-                                  color: Colors.white,
-                                )
-                              : Text(
-                                  'Create',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 0.025 * screenHeight,
-                                      color: Colors.white),
-                                ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter the organization name';
+                            }
+                            return null;
+                          },
+                          onTapOutside: (_) {
+                            FocusManager.instance.primaryFocus?.unfocus();
+                          },
                         ),
                       ),
-                    ),
-                  ],
+                      // Error Message (if any)
+                      if (orgController.errorMessage != null)
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 0.01 * screenHeight,
+                              horizontal: 0.07 * screenWidth),
+                          child: Text(
+                            orgController.errorMessage!,
+                            style: const TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      // Create Button
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 0.03 * screenHeight, bottom: 0.03 * screenHeight
+                        ),
+                        child: Container(
+                          height: 0.07 * screenHeight,
+                          width: (5 / 10) * screenWidth,
+                          decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                              colors: [
+                                AppColors.primary,
+                                AppColors.secondary,
+                              ],
+                              begin: FractionalOffset(0.0, 0.0),
+                              end: FractionalOffset(1.0, 0.0),
+                              stops: [0.0, 1.0],
+                              tileMode: TileMode.clamp,
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: Colors.transparent,
+                            ),
+                            onPressed: orgController.isLoading
+                                ? null
+                                : () {
+                                    if (_formKey.currentState!.validate()) {
+                                      orgController.createOrganization(
+                                        _orgNameController.text,
+                                        context,
+                                      );
+                                    }
+                                  },
+                            child: orgController.isLoading
+                                ? const CircularProgressIndicator(
+                                    color: Colors.white,
+                                  )
+                                : Text(
+                                    'Create',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 0.025 * screenHeight,
+                                        color: Colors.white),
+                                  ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
