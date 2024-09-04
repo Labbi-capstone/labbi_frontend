fa19e151ccfc0c55323adaade7a5b85799b438b5
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2024-09-04 21:06:38 +0700

    fix all MyButton not recognize when tapping on button background in login_page.dart, start_page.dart, register_page.dart and button.dart

--- lib/app/components/buttons/button.dart
+++ lib/app/components/buttons/button.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  final Widget text; // Change Text to Widget
+  // final Widget text; // Change Text to Widget
   final Function()? onTap;
-
-  const MyButton({super.key, required this.text, required this.onTap});
+final Widget child;
+  const MyButton({super.key, required this.onTap, required this.child});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Center(child: text),
+      child: child,
     );
   }
 }
 
+// Center(child: text)
